511d38ee4c5f2a48ec01ec918809fc3d6ef915fd
Mark Yalenti <myalenti@mongodb.com>
2016-11-23 15:29:28 -0500

    minor changes, cleanup mainly

--- ManageEC2Instances/py/UpdateExpiration.py
+++ ManageEC2Instances/py/UpdateExpiration.py
@@ -11,6 +11,25 @@ if __name__ == '__main__':
 import boto3
 import datetime
 import getopt, sys
+#Setting Defaults
+days=10
+pattern='myalenti*'
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "d:")
+except getopt.GetoptError:
+    print "You provided invalid command line switches."
+    #usage()
+    exit(2)
+
+    
+for opt, arg in opts:
+    #print "Tuple is " , opt, arg
+    if opt in ("-d"):
+        days=int(arg)
+    else:
+        print "Invalid Parameters detected"
+        #usage()
+        exit(2)    
 
 def getTagValue(tagName, tagArray):
     for i in tagArray:
@@ -18,33 +37,30 @@ def getTagValue(tagName, tagArray):
             return i['Value']
         
 def getInstances(prefix):
-    instances = clientEc2.describe_instances( Filters=[ {'Name' : 'tag:Name', 'Values' : [ 'myalenti*' ] }] )
-    print len(instances)
-    for elem in instances['Reservations']:
-        print(elem)
+    instances = clientEc2.describe_instances( Filters=[ {'Name' : 'tag:Name', 'Values' : [ prefix ] }] )
+    #print("Found the following number of instances to update: " + str(len(instances)))
     myInstances = []
     for elem in instances['Reservations']:
         for inst in elem['Instances']:
             instId =  inst['InstanceId']
             instName = getTagValue("Name", inst['Tags'])
             #print inst['Tags']
-            print(getTagValue("expire-on", inst['Tags']))
-            mydate = datetime.date.today() + datetime.timedelta(30) 
-            print(instId + " , " + instName + " , " + str(mydate))
-            r = { "instanceId" : instId , "instName" : instName, "expireDate" : str(mydate)}
+            #print(getTagValue("expire-on", inst['Tags']))
+            mydate = getTagValue("expire-on", inst['Tags']) 
+            print(instId + " , " + instName + " , " + mydate)
+            r = { "instanceId" : instId , "instName" : instName, "expireDate" : mydate}
             myInstances.append(r)
     return myInstances
 
 
 clientEc2 = boto3.client('ec2')
-myInstances = getInstances("myalenti")
-print(len(myInstances))
-print(myInstances)
-mydate = datetime.date.today() + datetime.timedelta(30) 
+myInstances = getInstances(pattern)
+mydate = datetime.date.today() + datetime.timedelta(days) 
 instList = []
 for i in myInstances:
     instList.append( i['instanceId'])
-print(instList)
+#print(instList)
 
 resp = clientEc2.create_tags(Resources=instList, Tags=[ { "Key" : "expire-on" , "Value" : str(mydate)}])
 print(resp['ResponseMetadata'])
+myInstances = getInstances(pattern)
